5fe8552204466fc842556656dadae3616e6fc1be
Florencio Varela <florenciovarela@avalith.net>
2019-01-31 13:04:58 -0300

    Navigation component was added

---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navigation from './Navigation.css'
+
+const Navigation = () => {
+    return (
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p>Sign Out</p>
+        </nav>
+    )
+}
+
+export default Navigation
